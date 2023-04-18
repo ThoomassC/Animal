bd8e70c9723340e1f7af055e71ebb65620fdab90
Thomas.C <thomas.caron@viacesi.fr>
2023-04-18 14:37:44 +0200

    feat  constructeur

--- Animal/Models/Griffin.cs
+++ Animal/Models/Griffin.cs
@@ -1,8 +1,9 @@
 ﻿namespace Animal.Models
 {
-    public class Griffin : AnimalMooveSleep
+    public class Griffin : Animal
     {
-        public Griffin() { }
+        public Griffin(int id, string name, string rarity) : base(id, name, rarity)
+        { }
 
         public override void move(string name)
         {
@@ -12,6 +13,11 @@
         public new void sleep(string name)
         {
             Console.WriteLine($"{name} dort ! (fonction new Griffin)");
+        }
+
+        public new void printIdAndNameAndRarity(int id, string name, string rarity)
+        {
+            Console.WriteLine($"l'id {id} a pour nom {name} et est de rareté {rarity}");
         }
     }
 }
--- Animal/Program.cs
+++ Animal/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using Animal.Models;
-using Animal.Interface;
 
 class Program
 {
@@ -8,17 +7,37 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
-        Griffin griffin= new();
+        /*Griffin griffin= new();
         griffin.move("Griffin");
-        griffin.sleep("Griffin");
+        griffin.sleep("Griffin");*/
 
-        Dragon dragon = new();
+        /*Dragon dragon = new();  // si constructeur vide
         dragon.move("Dragon");
-        dragon.sleep("Dragon");
+        dragon.sleep("Dragon");*/
 
-        Vehicule vehicule = new();
-        vehicule.move("Voiture");
+        var a = new Dragon(1, "Thomas Caron", "Legendaire");
 
-        Console.ReadLine();
+        IList<Dragon> animals = new List<Dragon>
+        {
+            new Dragon(1,"Thomas Caron", "Legendaire"), new Dragon(2, "Thomas Boucry", "commun"), new Dragon(3, "Samuel", "rare")
+        };
+         
+        //var execption = animals.Where(value => value.Rarity == "rare");
+        //var queryAnimal = from animal in animals where animal.Rarity == "rare" select animal;
+        /*foreach (var animal in queryAnimal)
+        {
+            animal.printIdAndNameAndRarity(animal.Id, animal.Name, animal.Rarity);
+        }*/
+            //foreach (var animal in execption) { animal.printIdAndNameAndRarity(animal.Id, animal.Name, animal.Rarity); }
+            // foreach(var animal in animals) { animal.printIdAndNameAndRarity(animal.Id, animal.Name, animal.Rarity); }
+
+            /*animal.printIdAndNameAndRarity(1, "Thomas Boucry", "commun");
+            animal.printIdAndNameAndRarity(1, "Thomas Caron", "epique");
+            animal.printIdAndNameAndRarity(1, "Samuel", "rare");*/
+
+            /*Vehicule vehicule = new();
+            vehicule.move("Voiture");*/
+
+            Console.ReadLine();
     }
 }
